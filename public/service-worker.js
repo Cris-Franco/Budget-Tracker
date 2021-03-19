e760a508f3c5c65714f60037c411bc2e7b67076d
Cris Franco <cris.franco219@gmail.com>
2021-03-19 14:30:57 -0400

    created install event listener

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,12 @@ let urlsToCache = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
